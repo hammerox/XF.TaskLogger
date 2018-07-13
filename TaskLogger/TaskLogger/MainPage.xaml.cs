d4f531753e4f75af38981a7c701070c429320899
mcustodio <mauricioramalho2@gmail.com>
2018-07-12 22:04:52 -0300

    Created Activity model and mocked list.

--- TaskLogger/TaskLogger/MainPage.xaml.cs
+++ TaskLogger/TaskLogger/MainPage.xaml.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using TaskLogger.Models;
 
 namespace TaskLogger
 {
 	public partial class MainPage : ContentPage
 	{
+        public List<Activity> activityList;
+
+
 		public MainPage()
 		{
 			InitializeComponent();
+            activityList = MockedList();
 		}
+
+        private List<Activity> MockedList()
+        {
+            var list = new List<Activity>();
+            list.Add(new Activity("Gym", "Byceps, Quads and Abs", new DateTime(1531443341), false));
+            list.Add(new Activity("Shopping", "I need to buy a lot of clothes", new DateTime(1530665741), true));
+            return list;
+        }
 	}
 }
---
+++ TaskLogger/TaskLogger/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskLogger.Models
+{
+    public class Activity
+    {
+        public string name;
+        public string description;
+        public DateTime date;
+        public bool isCompleted;
+
+        public Activity(string name, string description, DateTime date, bool isCompleted)
+        {
+            this.name = name;
+            this.description = description;
+            this.date = date;
+            this.isCompleted = isCompleted;
+        }
+    }
+}
