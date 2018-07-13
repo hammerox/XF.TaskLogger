2c2623f8043461c8b6c1183b3cf43ab7cfceadd0
mcustodio <mauricioramalho2@gmail.com>
2018-07-12 22:47:20 -0300

    Added databinding to name property.

--- TaskLogger/TaskLogger/MainPage.xaml.cs
+++ TaskLogger/TaskLogger/MainPage.xaml.cs
@@ -5,25 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using TaskLogger.Models;
+using System.Collections.ObjectModel;
 
 namespace TaskLogger
 {
 	public partial class MainPage : ContentPage
 	{
-        public List<Activity> activityList;
+        public ObservableCollection<Activity> ActivityList { get; private set; }
 
 
-		public MainPage()
+        public MainPage()
 		{
 			InitializeComponent();
-            activityList = MockedList();
+            ActivityList = MockedList();
+            //ActivityView.ItemsSource = ActivityList;
+            BindingContext = this;
 		}
 
-        private List<Activity> MockedList()
+        private ObservableCollection<Activity> MockedList()
         {
-            var list = new List<Activity>();
-            list.Add(new Activity("Gym", "Byceps, Quads and Abs", new DateTime(1531443341), false));
-            list.Add(new Activity("Shopping", "I need to buy a lot of clothes", new DateTime(1530665741), true));
+            var list = new ObservableCollection<Activity>();
+            list.Add(new Activity()
+            {
+                Name = "Gym",
+                Description = "Byceps, Quads and Abs",
+                Date = new DateTime(1531443341),
+                IsCompleted = false
+            });
+            list.Add(new Activity()
+            {
+                Name = "Shopping",
+                Description = "I need to buy a lot of clothes",
+                Date = new DateTime(1530665741),
+                IsCompleted = true
+            });
             return list;
         }
 	}
---
+++ TaskLogger/TaskLogger/Models/Activity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskLogger.Models
+{
+    public class Activity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
